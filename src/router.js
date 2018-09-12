44456f0ca5925cff1e291a4593a67166371a993c
Nguyen Ho <hothiennguyen124@gmail.com>
2018-09-12 20:11:14 +0700

    Update git ignore

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import requireAuth from './components/hoc/auth.js'
 import { BrowserRouter, Route, HashRouter } from 'react-router-dom'
 import Home from './components/home/container'
 const router = (
   <HashRouter>
-    <Route component={Home} path={'/'} />
+    <Route component={requireAuth(Home)} path={'/'} />
   </HashRouter>
 )
 export default router
